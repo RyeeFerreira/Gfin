2e58618a670ffb197a9594ba6fcc5273dc9d0f25
Geovanna Lessa <geovannalessa2@gmail.com>
2025-01-11 18:00:46 -0300

    Foi feito a tela de criar conta e sua estilizacao

--- src/pages/cadastrar/index.tsx
+++ src/pages/cadastrar/index.tsx
@@ -1,18 +1,185 @@
-import React from "react";
+import React, { useState, useEffect} from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    Image,
+    TouchableOpacity,
+    Alert,
+  } from 'react-native';
 
-import { Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { Ionicons } from '@expo/vector-icons';
 import { styles } from './styles';
-import { useFonts } from 'expo-font';
 import Logo from "../../assets/images/Gfin.png";
+import { LinearGradient } from 'expo-linear-gradient';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RouteProp } from '@react-navigation/native';
 
+//#region NavegaçãoConfig
+type RootStackParamList = {
+    Login: undefined;
+    Cadastrar: undefined;
+};
 
-export default function cadastrar() {
-    return (
-        <View>
+type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Cadastrar'>;
+type LoginScreenRouteProp = RouteProp<RootStackParamList, 'Cadastrar'>;
+
+type Props = {
+    navigation: LoginScreenNavigationProp;
+    route: LoginScreenRouteProp;
+};
+//#endregion
+
+export default function CriarConta({ navigation }: Props) {
+  const [senhaVisivel, setSenhaVisivel] = useState(false);
+  const [repetirSenhaVisivel, setRepetirSenhaVisivel] = useState(false);
+
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [repetirSenha, setRepetirSenha] = useState("");
+  const [isDisabled, setIsDisabled] = useState(true);
+
+    
+ useEffect(() => {
+   if (nome.trim() === '' || email.trim() === '' || senha.trim() === '' || repetirSenha.trim() === '') {
+        setIsDisabled(true); 
+    } else {
+        setIsDisabled(false); 
+    }
+}, [nome, email, senha, repetirSenha]);
+
+  const validarCampos = () => {
+    if (!nome.trim()) {
+      Alert.alert("Erro", "O campo Nome é obrigatório.");
+      return false;
+    }
+    if (!email.trim()) {
+      Alert.alert("Erro", "O campo E-mail é obrigatório.");
+      return false;
+    }
+    if (!senha.trim()) {
+      Alert.alert("Erro", "O campo Senha é obrigatório.");
+      return false;
+    }
+    if (senha !== repetirSenha) {
+      Alert.alert("Erro", "As senhas não coincidem.");
+      return false;
+    }
+    return true;
+  };
+
+  const cadastrar = () => {
+    if (validarCampos()) {
+      Alert.alert("Sucesso", "Conta criada com sucesso!");
+      navigation.navigate("Login");
+    }
+  };
+
+    return ( 
+    <LinearGradient 
+    colors={['#14130D','#14130D', '#14130D','#2E2A2A']} 
+    start={{x:0.00, y:0}}
+    end={{x:1, y:1}}
+    style={styles.linearGradient} >
+      <View style={styles.container}>
+        <View style={styles.header}>
+            <View>
+                {/* Botão Voltar */}
+                <TouchableOpacity onPress={() => navigation.navigate('Login')} style={styles.voltar}>
+                    <Ionicons name="arrow-back" size={20} color="#005E72" />
+                    <Text style={styles.voltarTexto}>VOLTAR</Text>
+                </TouchableOpacity>
+                
+                {/* Titulo */}
+                <Text style={styles.titulo}>Criar Conta</Text>
+            </View>
+
+            {/* Logo */}
+            <View>
+                <Image source={Logo} style={styles.logo} />
+            </View>
         </View>
+           
+            
 
-    )
+        {/* Formulário */}
+        <View style={styles.form}>
+          {/* Nome */}
+          <Text style={styles.label}>Nome</Text>
+          <TextInput
+            value={nome}
+            onChangeText={setNome}
+            style={[styles.input, !nome.trim() && styles.inputError]}
+            placeholder="Digite seu nome"
+            placeholderTextColor="#7B7B7B"
+          />
+  
+          {/* E-mail */}
+          <Text style={styles.label}>E-mail</Text>
+          <TextInput
+            value={email}
+            onChangeText={setEmail}
+            style={styles.input}
+            placeholder="Digite seu e-mail"
+            placeholderTextColor="#7B7B7B"
+            keyboardType="email-address"
+          />
+  
+          {/* Senha */}
+          <Text style={styles.label}>Senha</Text>
+          <View style={styles.inputContainer}>
+            <TextInput
+              value={senha}
+              onChangeText={setSenha}
+              style={styles.inputComIcone}
+              placeholder="Digite uma senha"
+              placeholderTextColor="#7B7B7B"
+              secureTextEntry={!senhaVisivel}
+            />
+            <TouchableOpacity
+              onPress={() => setSenhaVisivel(!senhaVisivel)}
+              style={styles.icon}>
+              <Ionicons
+                name={senhaVisivel ? 'eye' : 'eye-off'}
+                size={20}
+                color="#A6A6A6"
+              />
+            </TouchableOpacity>
+          </View>
+  
+          {/* Repetir Senha */}
+          <Text style={styles.label}>Repita a Senha</Text>
+          <View style={styles.inputContainer}>
+            <TextInput
+            value={repetirSenha}
+            onChangeText={setRepetirSenha}
+              style={styles.inputComIcone}
+              placeholder="Digite novamente uma senha"
+              placeholderTextColor="#A6A6A6"
+              secureTextEntry={!repetirSenhaVisivel}
+            />
+            <TouchableOpacity
+              onPress={() => setRepetirSenhaVisivel(!repetirSenhaVisivel)}
+              style={styles.icon}>
+              <Ionicons
+                name={repetirSenhaVisivel ? 'eye' : 'eye-off'}
+                size={20}
+                color="#A6A6A6"
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+  
+        {/* Botão Cadastrar */}
+        <View style={styles.btn}>
+        <TouchableOpacity onPress={cadastrar} style={[styles.botaoCadastrar, isDisabled && styles.buttonDisabled]}>
+          <Text style={styles.botaoTexto}>CADASTRAR</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
+      </LinearGradient>
+    );
 }
-
+  
+  
--- src/pages/cadastrar/styles.ts
+++ src/pages/cadastrar/styles.ts
@@ -1,8 +1,124 @@
 import { StyleSheet} from "react-native";
-import { useFonts } from 'expo-font';
 
 
 
 export const styles = StyleSheet.create({
+    inputError: {
+        borderColor: "red",
+    },
 
-});
+    linearGradient: {
+        flex: 1,
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: 5
+    },
+
+    container: {
+      flex: 1, 
+      padding: 25,
+      marginTop: 30,
+    },
+
+    header: {
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+    },
+
+    voltar: {
+      flexDirection: 'row',
+    },
+    
+    voltarTexto: {
+      fontSize: 20,
+      marginLeft: 5,
+      color: '#FFFFFF',
+      fontFamily: "InrianSans",
+    },
+
+    logo: {
+      width: 45,
+      height: 45,
+      borderRadius: 100,
+      alignItems: 'center', 
+      marginTop:14
+    },
+
+    titulo: {
+      fontSize: 37,
+      textAlign: 'left',
+      color: '#FFFFFF',
+      marginBottom: '60%',
+      marginTop: 8,
+      fontFamily: "InrianSans",
+    },
+
+    form: {
+      marginBottom: 30,
+    }, 
+
+    label: {
+        color: '#FFFFFF', // Cor do texto do label
+        fontSize: 16,
+        marginBottom: 8, 
+        fontWeight: 'light',
+        fontFamily: "InrianSans",
+    },
+
+    input: {
+      backgroundColor: '#1A1913',
+      color: '#D2D2D2',
+      borderRadius: 8,
+      paddingHorizontal: 16,
+      paddingVertical: 15,
+      marginBottom: 30,
+      fontFamily: "InrianSans",
+      fontSize: 16,
+    },
+
+    inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: '#1A1913',
+      borderRadius: 8,
+      marginBottom: 30,
+      fontFamily: "InrianSans",
+    },
+
+    inputComIcone: {
+      flex: 1,
+      color: '#D2D2D2',
+      paddingHorizontal: 15,
+      paddingVertical: 15,
+      fontFamily: "InrianSans",
+    },
+    
+    icon: {
+      paddingHorizontal: 10,
+    }, 
+
+    btn: {
+        alignSelf: 'flex-end',
+        width:'50%',
+    },
+
+    botaoCadastrar: {
+      backgroundColor: '#034E5E',
+      paddingVertical: 15,
+      borderRadius: 6,
+      marginTop:'10%',
+      //width:'50%',
+      alignItems: 'center',
+    },
+
+    botaoTexto: {
+      color: '#D2D2D2',
+      fontSize: 16,
+      fontWeight: 'bold',
+      fontFamily: "InrianSans"
+    },
+
+    buttonDisabled:{
+        backgroundColor: '#5A5A5A',
+    }
+  });
