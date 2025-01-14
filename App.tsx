1c86d22b17082f5f511010e3587465991bed878b
RyeeFerreira <ryeeferreira@gmail.com>
2025-01-13 21:50:13 -0300

    usuarios database criado

--- App.tsx
+++ App.tsx
@@ -3,8 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useFonts } from 'expo-font';
 import { ActivityIndicator } from 'react-native';
-import { initializeApp } from "firebase/app";
-import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore';
 
 
 import Login from './src/pages/login';
@@ -12,19 +10,7 @@ import Senha from './src/pages/esqueceuSenha';
 import Cadastrar from './src/pages/cadastrar';
 import inicio from './src/pages/inicio';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAHbzTZHudGrSIznuX3_7gHDZ83CnXt98E",
-  authDomain: "bdgf-dd7b4.firebaseapp.com",
-  projectId: "bdgf-dd7b4",
-  storageBucket: "bdgf-dd7b4.firebasestorage.app",
-  messagingSenderId: "103024531131",
-  appId: "1:103024531131:web:26579b5fec24c3deaa3b9e",
-  measurementId: "G-P533GDX2TM"
-};
 
-// Inicialize o Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 // Defina os tipos das rotas
 type RootStackParamList = {
