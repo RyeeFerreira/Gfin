b18173aba5c670ff0e50680b4314e79d9fbd9c05
RyeeFerreira <ryeeferreira@gmail.com>
2025-01-15 19:38:29 -0300

    função logar

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,6 +5,7 @@ import { styles } from './styles';
 import Logo from "../../assets/images/Gfin.png";
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RouteProp } from '@react-navigation/native';
+import { useUsuarioDatabase } from "../../database/useUsuarioDatabase";
 
 // Define os tipos das rotas
 type RootStackParamList = {
@@ -31,66 +32,78 @@ export default function Login({ navigation }: Props) {
     const [senha, setSenha] = useState('');
     const [isDisabled, setIsDisabled] = useState(true);
 
-    
+    const usuarioDatabase = useUsuarioDatabase();
+
+    async function logar() {
+        try {
+            if (!isDisabled) {
+                const respose = await usuarioDatabase.consulta(email, senha)
+                if (respose == true) {
+                    return navigation.navigate('Inicio');
+                } else {
+                    return   Alert.alert("Usuario não encontrado")
+            } 
+                    Alert.alert("Por favor, preencha os campos antes de continuar.");
+            }
+
+            }catch (error) {
+                Alert.alert("Erro ao logar")
+                console.log(error)
+            }
+        }
+
     useEffect(() => {
-        if (email.trim() === '' || senha.trim() === '') {
-            setIsDisabled(true); 
-        } else {
-            setIsDisabled(false); 
-        }
-    }, [email, senha]);
+            if (email.trim() === '' || senha.trim() === '') {
+                setIsDisabled(true);
+            } else {
+                setIsDisabled(false);
+            }
+        }, [email, senha]);
 
-    const logar = () => {
-        if (!isDisabled) {
-            navigation.navigate('Inicio');
-        } else {
-            Alert.alert("Por favor, preencha os campos antes de continuar.");
-        }
-    };
 
-    return (
-        <View style={styles.container}>
-            <Image style={styles.logo} source={Logo} />
+        return (
+            <View style={styles.container}>
+                <Image style={styles.logo} source={Logo} />
 
-            <View style={styles.grupo1}>
-                <Text style={styles.label}>Email:</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Digite seu email"
-                    placeholderTextColor="#AEA8A8"
-                    value={email}
-                    onChangeText={setEmail} // Atualiza o estado do email
-                />
+                <View style={styles.grupo1}>
+                    <Text style={styles.label}>Email:</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Digite seu email"
+                        placeholderTextColor="#AEA8A8"
+                        value={email}
+                        onChangeText={setEmail} // Atualiza o estado do email
+                    />
+                </View>
+
+                <View style={styles.grupo2}>
+                    <Text style={styles.label}>Senha:</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Digite sua senha"
+                        placeholderTextColor="#AEA8A8"
+                        value={senha}
+                        onChangeText={setSenha}
+                        secureTextEntry
+                    />
+                </View>
+
+                <View style={styles.grupo3}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Senha')}>
+                        <Text style={styles.link}>Esqueceu sua senha?</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate('Cadastrar')}>
+                        <Text style={styles.link}>Cadastrar</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <TouchableHighlight
+                    style={[styles.button, isDisabled && styles.buttonDisabled]}
+                    disabled={isDisabled}
+                    onPress={logar}
+                >
+                    <Text style={styles.buttonText}>ENTRAR</Text>
+                </TouchableHighlight>
             </View>
-
-            <View style={styles.grupo2}>
-                <Text style={styles.label}>Senha:</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Digite sua senha"
-                    placeholderTextColor="#AEA8A8"
-                    value={senha}
-                    onChangeText={setSenha} // Atualiza o estado da senha
-                    secureTextEntry // Oculta o texto para senhas
-                />
-            </View>
-
-            <View style={styles.grupo3}>
-                <TouchableOpacity onPress={() => navigation.navigate('Senha')}>
-                    <Text style={styles.link}>Esqueceu sua senha?</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate('Cadastrar')}>
-                    <Text style={styles.link}>Cadastrar</Text>
-                </TouchableOpacity>
-            </View>
-
-            <TouchableHighlight
-                style={[styles.button, isDisabled && styles.buttonDisabled]} // Aplica estilo para desabilitado
-                disabled={isDisabled}
-                onPress={logar}
-            >
-                <Text style={styles.buttonText}>ENTRAR</Text>
-            </TouchableHighlight>
-        </View>
-    );
-}
+        );
+    }
