bdb5f9aa656e2a81bb7c7d0884a4320e446e9cc4
Geovanna Lessa <geovannalessa2@gmail.com>
2025-01-28 18:29:15 -0300

    Adicionando para as telas a possibilidade de rolagem da tela, e alterações no layout

--- src/pages/cadastrar/styles.ts
+++ src/pages/cadastrar/styles.ts
@@ -3,129 +3,115 @@ import { StyleSheet} from "react-native";
 
 
 export const styles = StyleSheet.create({
-    inputError: {
-        borderColor: "red",
-    },
+  scrollContent: {
+    flexGrow: 1, // Garante que o conteúdo ocupe espaço para rolagem
+  },
 
-    linearGradient: {
-        flex: 1,
-        paddingLeft: 15,
-        paddingRight: 15,
-        borderRadius: 5
-    },
+  inputError: {
+    borderColor: "red",
+  },
 
-    container: {
-      flex: 1, 
-      padding: 25,
-      marginTop: 30,
-    },
+  linearGradient: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5
+  },
 
-    header: {
-        flexDirection: 'row', 
-        justifyContent: 'space-between', 
-    },
+  container: {
+    flex: 1,
+    padding: 25,
+    marginTop: '10%',// rever aq
+  },
 
-    voltar: {
-      flexDirection: 'row',
-    },
-    
-    voltarTexto: {
-      fontSize: 20,
-      marginLeft: 5,
-      color: '#FFFFFF',
-      fontFamily: "InrianSans",
-    },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 
-    logo: {
-      width: 45,
-      height: 45,
-      borderRadius: 100,
-      alignItems: 'center', 
-      marginTop:14
-    },
+  voltar: {
+    flexDirection: 'row',
+  },
 
-    titulo: {
-      fontSize: 37,
-      textAlign: 'left',
-      color: '#FFFFFF',
-      marginBottom: '60%',
-      marginTop: 8,
-      fontFamily: "InrianSans",
-    },
+  voltarTexto: {
+    fontSize: 20,
+    marginLeft: 5,
+    color: '#FFFFFF',
+    fontFamily: "InrianSans",
+  },
 
-    form: {
-      marginBottom: 30,
-    }, 
+  titulo: {
+    fontSize: 37,
+    textAlign: 'left',
+    color: '#FFFFFF',
+    marginBottom: '60%',
+    marginTop: 8,
+    fontFamily: "InrianSans",
+  },
 
-    label: {
-        color: '#FFFFFF', // Cor do texto do label
-        fontSize: 16,
-        marginBottom: 8, 
-        fontWeight: 'light',
-        fontFamily: "InrianSans",
-    },
+  form: {
+    marginBottom: '2%',
+  },
 
-    input: {
-      backgroundColor: '#1A1913',
-      color: '#D2D2D2',
-      borderRadius: 8,
-      paddingHorizontal: 16,
-      paddingVertical: 15,
-      marginBottom: 1,
-      fontFamily: "InrianSans",
-      fontSize: 16,
-    },
+  label: {
+    color: '#FFFFFF', // Cor do texto do label
+    fontSize: 16,
+    marginBottom: '5%',
+    fontWeight: 'light',
+    fontFamily: "InrianSans",
+  },
 
-    inputContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      backgroundColor: '#1A1913',
-      borderRadius: 8,
-      marginBottom: 1,
-      fontFamily: "InrianSans",
-    },
+  input: {
+    backgroundColor: '#1A1913',
+    color: '#D2D2D2',
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingVertical: 15,
+    marginBottom: '5%',
+    fontFamily: "InrianSans",
+    fontSize: 16,
+  },
 
-    inputComIcone: {
-      flex: 1,
-      color: '#D2D2D2',
-      paddingHorizontal: 15,
-      paddingVertical: 15,
-      fontFamily: "InrianSans",
-    },
-    
-    icon: {
-      paddingHorizontal: 10,
-    }, 
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#1A1913',
+    borderRadius: 8,
+    marginBottom: '5%',
+    fontFamily: "InrianSans",
+  },
 
-    btn: {
-      backgroundColor: "#0097B2",
-      borderColor:  "#0097C0",
-      borderWidth: 0.5,
-      width: "70%",
-      height: "7%",
-      marginTop: "15%",
-      borderRadius: 100,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
+  inputComIcone: {
+    flex: 1,
+    color: '#D2D2D2',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    fontFamily: "InrianSans",
+  },
 
-    botaoCadastrar: {
-      backgroundColor: '#034E5E',
-      paddingVertical: 15,
-      borderRadius: 6,
-      marginTop:'10%',
-      //width:'50%',
-      alignItems: 'center',
-    },
+  icon: {
+    paddingHorizontal: 10,
+  },
 
-    botaoTexto: {
-      color: '#D2D2D2',
-      fontSize: 16,
-      fontWeight: 'bold',
-      fontFamily: "InrianSans"
-    },
+  botaoCadastrar: {
+    margin: 'auto',
+    backgroundColor: "#005E72",
+    width: "70%",
+    height: "23%",
+    marginTop: "15%",
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-    buttonDisabled:{
-        backgroundColor: '#5A5A5A',
-    }
+  botaoTexto: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+    fontFamily: "InrianSans"
+  },
+
+  buttonDisabled: {
+    backgroundColor: '#5A5A5A',
+  }
   });
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 
-import { Text, View, Image, TextInput, TouchableOpacity, TouchableHighlight, Alert } from 'react-native';
+import { Text, View, Image, TextInput, TouchableOpacity, TouchableHighlight, Alert, ScrollView, Platform, KeyboardAvoidingView } from 'react-native';
 import { styles } from './styles';
 import Logo from "../../assets/images/Gfin.png";
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RouteProp } from '@react-navigation/native';
 import { useUsuarioDatabase } from "../../database/useUsuarioDatabase";
+import { LinearGradient } from 'expo-linear-gradient';
 
 // Define os tipos das rotas
 type RootStackParamList = {
@@ -41,68 +42,76 @@ export default function Login({ navigation }: Props) {
                 if (respose == true) {
                     return navigation.navigate('Inicio');
                 } else {
-                    return   Alert.alert("Usuario não encontrado")
-            }       
+                    return Alert.alert("Usuario não encontrado")
+                }
             }
 
-            }catch (error) {
-                Alert.alert("Erro ao logar")
-                console.log(error)
-            }
+        } catch (error) {
+            Alert.alert("Erro ao logar")
+            console.log(error)
         }
+    }
 
     useEffect(() => {
-            if (email.trim() === '' || senha.trim() === '') {
-                setIsDisabled(true);
-            } else {
-                setIsDisabled(false);
-            }
-        }, [email, senha]);
+        if (email.trim() === '' || senha.trim() === '') {
+            setIsDisabled(true);
+        } else {
+            setIsDisabled(false);
+        }
+    }, [email, senha]);
 
 
-        return (
-            <View style={styles.container}>
-                <Image style={styles.logo} source={Logo} />
+    return (
+        <KeyboardAvoidingView style={styles.scrollContent} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+            <ScrollView contentContainerStyle={styles.scrollContent}>
+                <LinearGradient
+                    colors={['#14130D', '#14130D', '#14130D', '#2E2A2A']}
+                    start={{ x: 0.00, y: 0 }}
+                    end={{ x: 1, y: 1 }}
+                    style={styles.linearGradient}>
+                    <View style={styles.container}>
+                        <Image style={styles.logo} source={Logo} />
 
-                <View style={styles.grupo1}>
-                    <Text style={styles.label}>Email:</Text>
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Digite seu email"
-                        placeholderTextColor="#AEA8A8"
-                        value={email}
-                        onChangeText={setEmail} // Atualiza o estado do email
-                    />
-                </View>
+                        <View style={styles.form}>
+                            <Text style={styles.label}>Email:</Text>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Digite seu email"
+                                placeholderTextColor="#AEA8A8"
+                                value={email}
+                                onChangeText={setEmail} 
+                            />
 
-                <View style={styles.grupo2}>
-                    <Text style={styles.label}>Senha:</Text>
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Digite sua senha"
-                        placeholderTextColor="#AEA8A8"
-                        value={senha}
-                        onChangeText={setSenha}
-                        secureTextEntry
-                    />
-                </View>
+                            <Text style={styles.label}>Senha:</Text>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Digite sua senha"
+                                placeholderTextColor="#AEA8A8"
+                                value={senha}
+                                onChangeText={setSenha} 
+                                secureTextEntry
+                            />
+                        </View>
 
-                <View style={styles.grupo3}>
-                    <TouchableOpacity onPress={() => navigation.navigate('Senha')}>
-                        <Text style={styles.link}>Esqueceu sua senha?</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => navigation.navigate('Cadastrar')}>
-                        <Text style={styles.link}>Cadastrar</Text>
-                    </TouchableOpacity>
-                </View>
+                        <View style={styles.grupo3}>
+                            <TouchableOpacity onPress={() => navigation.navigate('Senha')}>
+                                <Text style={styles.link}>Esqueceu sua senha?</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => navigation.navigate('Cadastrar')}>
+                                <Text style={styles.link}>Cadastrar</Text>
+                            </TouchableOpacity>
+                        </View>
 
-                <TouchableHighlight
-                    style={[styles.button, isDisabled && styles.buttonDisabled]}
-                    disabled={isDisabled}
-                    onPress={logar}
-                >
-                    <Text style={styles.buttonText}>ENTRAR</Text>
-                </TouchableHighlight>
-            </View>
-        );
-    }
+                        <TouchableHighlight
+                            style={[styles.button, isDisabled && styles.buttonDisabled]}
+                            disabled={isDisabled}
+                            onPress={logar}
+                        >
+                            <Text style={styles.buttonText}>ENTRAR</Text>
+                        </TouchableHighlight>
+                    </View>
+                </LinearGradient>
+            </ScrollView>
+        </KeyboardAvoidingView>
+    )
+}
